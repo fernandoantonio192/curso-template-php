e0c71b56abbf2b167e1bf00ae9bdfd6408dd9c45
fernandoantonio192 <92fernando.antonio@gmail.com>
2024-04-09 15:49:26 -0300

    add HomeController

---
+++ public/home.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Home</title>
+</head>
+<body>
+	<h1>Home Page</h1>
+	<p>Olá, <?php echo $name; ?>!</p>
+</body>
+</html>
--- public/index.php
+++ public/index.php
@@ -1,3 +1,19 @@
 <?php
 
-echo '<h1>Home</h1>';
+# output buffer (Buffer de saída)
+
+# como funciona
+
+$data = ["name" => "Fernando"];
+
+ob_start(); # activa o buffer de saída
+
+extract($data);
+
+require 'home.php';
+
+$content = ob_get_contents(); # captura o conteúdo entre ob_start() e ob_end_clean()
+
+ob_end_clean(); # encerra o buffer e limpa a memória
+
+var_dump($content);
